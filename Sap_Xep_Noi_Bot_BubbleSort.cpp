0c6bebc2e19eb2810c0cc293417ade2f6dd367c9
tranhieu899999@gmail.com <tranhieu899999@gmail.com>
2026-01-19 17:28:34 +0700

    change

--- Sap_Xep_Noi_Bot_BubbleSort.cpp
+++ Sap_Xep_Noi_Bot_BubbleSort.cpp
@@ -1,11 +1,7 @@
 clscls//Sap Xep Noi Bot: Bubble Sort
 //O(N^2)
 
-#include <stdio.h>
 
-void BubbleSort(int a[], int n){
-	for (int i = 0; i < (n - 1); i++){
-		//Duyet tu chi so j = 0 den j - i - 2
 		for (int j = 0; j < (n - i - 1); j++){
 			//So sanh hai phan tu dung canh nhau a[j] va a[j + 1]
 			if (a[j] > a[j + 1]){
